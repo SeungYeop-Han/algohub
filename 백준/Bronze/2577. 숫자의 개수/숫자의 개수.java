4a66094d3a1ce164a254f4ba60a5fbdccd266242
한승엽 <106862797+SeungYeop-Han@users.noreply.github.com>
2024-10-30 10:24:12 +0900

    [Bronze II] Title: 숫자의 개수, Time: 104 ms, Memory: 14236 KB -BaekjoonHub

--- 백준/Bronze/2577. 숫자의 개수/숫자의 개수.java
+++ 백준/Bronze/2577. 숫자의 개수/숫자의 개수.java
@@ -2,11 +2,6 @@ import java.io.IOException;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
-/**
- * 알파벳 개수(백준 10808; 브론즈4)
- * 시간 제한: 1초; 메모리 제한 128MB
- * 수학, 구현, 사칙연산
- */
 public class Main {
 
     public static void main(String[] args) throws IOException {
